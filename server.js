5331a8f7d6d46ade8cb414cf1600eb99eee05c72
Jatin Agrawal <jatinagrawal0801@gmail.com>
2024-04-20 17:46:33 -0700

    some changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 const cors=require('cors')
-const PORT= 8080 || process.env.PORT
+const PORT= process.env.PORT || 8080
 app.use(cors())
 
 app.get('/',(req,res)=>{
